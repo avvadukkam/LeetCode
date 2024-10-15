1a5932703c0526d2c70f4301922cf731bcec115f
avvadukkam <davvadukkam@gmail.com>
2024-10-15 15:31:21 -0400

    10/15/2024

--- 1251-1500/leetcode1496.py
+++ 1251-1500/leetcode1496.py
@@ -18,3 +18,25 @@ class Solution:
                 return True
             visited.add(current)
         return False
+    
+## OR
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        current = (0,0)
+        visited = set()
+        visited.add(current)
+        moves = {
+            'N': (0, 1),
+            'S': (0, -1),
+            'E': (1, 0),
+            'W': (-1, 0)
+        }
+        
+        for direction in path:
+            dx, dy = moves[direction]
+            current = (current[0] + dx, current[1] + dy)
+
+            if current in visited:
+                return True
+            visited.add(current)
+        return False

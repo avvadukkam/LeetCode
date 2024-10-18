0a0418e0ccabd0402d1bf7fd4a05f0d63ac40d7a
avvadukkam <davvadukkam@gmail.com>
2024-10-18 14:17:49 -0400

    10/18/2024

---
+++ 2251-2500/leetcode2481.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+        if n==1:
+            return 0
+        elif n%2==0:
+            return n//2
+        else:
+            return n
+        
+#One liner
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+        return 0 if n == 1 else n // 2 if n % 2 == 0 else n

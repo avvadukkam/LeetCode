9ce64cee115351cc773278cd39397fd924429020
avvadukkam <davvadukkam@gmail.com>
2024-10-19 01:05:59 -0400

    10/19/2024

--- 501-750/leetcode507.py
+++ 501-750/leetcode507.py
@@ -11,3 +11,19 @@ class Solution:
         if sumN == num:
             return True
         return False
+
+# OR
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <= 1:
+            return False
+        
+        sumN = 1
+        
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                sumN += i
+                if i != num // i: 
+                    sumN += num // i
+
+        return sumN == num

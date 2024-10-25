3cd12f92231bc5c6d35e3e766a1d07dc614fe393
avvadukkam <davvadukkam@gmail.com>
2024-10-24 22:21:27 -0400

    10/24/2024

---
+++ 1251-1500/leetcode1407.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select name, 
+sum(case when u.id = r.user_id then distance
+else 0 end) as travelled_distance
+from Users u left join Rides r
+on u.id = r.user_id
+group by u.id
+order by sum(distance) desc, name asc

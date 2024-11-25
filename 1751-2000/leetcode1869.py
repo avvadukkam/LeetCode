406c950e4ec93a91b80b8a30e10c3a463107636f
avvadukkam <davvadukkam@gmail.com>
2024-11-24 22:35:52 -0500

    11/24/2024

--- 1751-2000/leetcode1869.py
+++ 1751-2000/leetcode1869.py
@@ -6,3 +6,20 @@ class Solution:
         maxLenS1 = max(len(i) for i in s1)
 
         return maxLenS1 > maxLenS0
+    
+# OR
+class Solution:
+    def checkZeroOnes(self, s: str) -> bool:
+        maxLenS1 = maxLenS0 = currentS1 = currentS0 = 0
+        
+        for char in s:
+            if char == '1':
+                currentS1 += 1
+                currentS0 = 0
+                maxLenS1 = max(maxLenS1, currentS1)
+            else:
+                currentS0 += 1
+                currentS1 = 0 
+                maxLenS0 = max(maxLenS0, currentS0)
+        
+        return maxLenS1 > maxLenS0

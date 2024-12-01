781bce8765ad9d940cf89738866fb5633350aff3
avvadukkam <davvadukkam@gmail.com>
2024-12-01 00:25:17 -0500

    2/1/2024

---
+++ 1751-2000/leetcode1945.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        intS = ""
+        for c in s:
+            intS += str(ord(c)-96)
+       
+        while k != 0:
+            tranS = 0
+            for i in intS:
+                tranS += int(i)
+            intS = str(tranS)
+            k -= 1
+        return int(tranS)
+    
+# Optimized
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        intS = ''.join(str(ord(c) - 96) for c in s)
+        
+        while k > 0:
+            tranS = sum(int(digit) for digit in intS)
+            intS = str(tranS)
+            k -= 1
+        
+        return tranS

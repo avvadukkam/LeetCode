86ca48e5bf4672e4add409be1fbfec42d4d6c699
avvadukkam <davvadukkam@gmail.com>
2024-12-02 23:28:32 -0500

    12/02/2024

--- 1751-2000/leetcode1974.py
+++ 1751-2000/leetcode1974.py
@@ -6,3 +6,17 @@ class Solution:
             distance = abs(ord(word[i])-ord(word[i-1]))
             count += min(distance, 26-distance)
         return count
+    
+
+# Optimized
+class Solution:
+    def minTimeToType(self, word: str) -> int:
+        count = len(word)
+        prev = 'a'
+
+        for char in word:
+            distance = abs(ord(char)-ord(prev))
+            count += min(distance, 26-distance)
+            prev = char
+            
+        return count

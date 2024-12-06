a8375c8198328f950ecf92fed780b14383f37449
avvadukkam <davvadukkam@gmail.com>
2024-12-05 23:06:53 -0500

    12/05/2024

--- 2001-2250/leetcode2016.py
+++ 2001-2250/leetcode2016.py
@@ -1,3 +1,4 @@
+# Brute Force
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
         maxDiff = -1
@@ -7,3 +8,18 @@ class Solution:
                     maxDiff = max(maxDiff, nums[j] - nums[i])
 
         return maxDiff
+    
+
+# Optimized
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        min_value = nums[0]
+        max_diff = -1
+
+        for num in nums[1:]:
+            if num > min_value:
+                max_diff = max(max_diff, num - min_value)
+            else:
+                min_value = num
+        
+        return max_diff

dbace1fb7670f70c5e52396371c403147af4c361
avvadukkam <davvadukkam@gmail.com>
2024-12-05 22:52:37 -0500

    12/05/2024

--- 1751-2000/leetcode1995.py
+++ 1751-2000/leetcode1995.py
@@ -1,3 +1,4 @@
+# Brute Force # O(n^3)
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
         n = len(nums)
@@ -6,7 +7,23 @@ class Solution:
             for j in range(i+1, n-2):
                 for k in range(j+1, n-1):
                     sumD = nums[i] + nums[j] + nums[k]
-                    if nums[i] + nums[j] + nums[k] in nums[k+1:]:
+                    if sumD in nums[k+1:]:
                         count += nums[k+1:].count(sumD)
 
+        return count
+    
+# Optimized # O(n^2)
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        count = 0
+        n = len(nums)
+        freq = defaultdict(int)
+        
+        for k in range(n - 1, 1, -1):
+            freq[nums[k]] += 1
+            for j in range(k - 1, 0, -1):
+                for i in range(j - 1, -1, -1):
+                    sumD = nums[i] + nums[j] + nums[k]
+                    count += freq[sumD]
+        
         return count

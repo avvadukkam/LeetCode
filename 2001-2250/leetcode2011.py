5ac42f59a11c4e847675159402972a1d8748a7f2
avvadukkam <davvadukkam@gmail.com>
2024-12-05 22:59:01 -0500

    12/05/2024

--- 2001-2250/leetcode2011.py
+++ 2001-2250/leetcode2011.py
@@ -8,3 +8,7 @@ class Solution:
                 x -= 1
         return x
     
+# optimized
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        return sum(1 if '+' in op else -1 for op in operations)

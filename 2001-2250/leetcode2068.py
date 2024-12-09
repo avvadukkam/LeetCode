845acb30eb4bf2eab1a99fbc9aa2ea26b3fc8a51
avvadukkam <davvadukkam@gmail.com>
2024-12-08 23:16:06 -0500

    12/08/2024

---
+++ 2001-2250/leetcode2068.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        word1_dict = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+        word2_dict = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+        for letter in word1:
+            word1_dict[letter] += 1
+        for letter in word2:
+            word2_dict[letter] += 1
+        
+        for word, count in word1_dict.items():
+            if abs(word1_dict[word] - word2_dict[word]) >3:
+                return False
+        return True
+    

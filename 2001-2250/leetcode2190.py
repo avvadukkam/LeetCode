64c0cbeb3c315316708044843dd58547ab0a95b5
avvadukkam <davvadukkam@gmail.com>
2024-12-19 17:24:18 -0500

    12/19/2023

--- 2001-2250/leetcode2190.py
+++ 2001-2250/leetcode2190.py
@@ -6,3 +6,15 @@ class Solution:
                 count[nums[i+1]] += 1
                 
         return max(count, key=count.get)
+    
+
+# OR
+class Solution:
+    def mostFrequent(self, nums: List[int], key: int) -> int:
+        count = {}
+        for i in range(len(nums)-1):
+            if nums[i] == key:
+                target = nums[i + 1]
+                count[target] = count.get(target, 0) + 1
+                
+        return max(count, key=count.get)

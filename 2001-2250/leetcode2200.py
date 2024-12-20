bb9995cb498be2617f57f4b4948d7354629265ba
avvadukkam <davvadukkam@gmail.com>
2024-12-20 11:55:59 -0500

    12/20/2024

--- 2001-2250/leetcode2200.py
+++ 2001-2250/leetcode2200.py
@@ -13,3 +13,14 @@ class Solution:
 
         return res
 
+# Optimized
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        key_list = [i for i, num in enumerate(nums) if num == key]
+        
+        res = []
+        for i in range(len(nums)):
+            if any(abs(i - j) <= k for j in key_list):
+                res.append(i)
+        
+        return res

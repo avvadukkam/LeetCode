73556c42902661c05816879e140d640c79d35e19
avvadukkam <davvadukkam@gmail.com>
2024-12-21 22:49:13 -0500

    12/21/2024

--- 2001-2250/leetcode2210.py
+++ 2001-2250/leetcode2210.py
@@ -9,3 +9,19 @@ class Solution:
                 elif nums[i-1] > nums[i] and nums[i] < nums[i+1]: #valley
                     count += 1
         return count
+    
+
+# Optimized
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        nums = [nums[i] for i in range(len(nums) - 1) if nums[i] != nums[i + 1]] + [nums[-1]]
+        
+        count = 0
+        
+        for i in range(1, len(nums) - 1):
+            if nums[i - 1] < nums[i] > nums[i + 1]:  # Hill
+                count += 1
+            elif nums[i - 1] > nums[i] < nums[i + 1]:  # Valley
+                count += 1
+                
+        return count

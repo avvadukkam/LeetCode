f745afede84fd1a7d0286f14c153fa8df0801844
avvadukkam <davvadukkam@gmail.com>
2024-12-21 22:47:25 -0500

    12/21/2024

---
+++ 2001-2250/leetcode2210.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        count = 0
+        nums = [nums[i] for i in range(len(nums) - 1) if nums[i] != nums[i + 1]] + [nums[-1]]
+        for i in range(1,len(nums)-1):
+            if nums[i] != nums[i-1] or nums[i] != nums[i+1]:
+                if nums[i-1] < nums[i] and nums[i] > nums[i+1]: # hill
+                    count += 1
+                elif nums[i-1] > nums[i] and nums[i] < nums[i+1]: #valley
+                    count += 1
+        return count

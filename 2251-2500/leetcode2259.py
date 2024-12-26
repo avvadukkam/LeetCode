7fe279e469fdea92c19b68171293aa258b90b6fc
avvadukkam <davvadukkam@gmail.com>
2024-12-25 21:48:14 -0500

    12/25/2024

--- 2251-2500/leetcode2259.py
+++ 2251-2500/leetcode2259.py
@@ -5,3 +5,8 @@ class Solution:
             if number[i] == digit:
                 res.append(number[:i] + number[i+1:])
         return max(res)
+    
+# One-liner
+class Solution:
+    def removeDigit(self, number: str, digit: str) -> str:
+        return max(number[:i] + number[i+1:] for i in range(len(number)) if number[i]==digit)

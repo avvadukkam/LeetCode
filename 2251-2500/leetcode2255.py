9efde7387cda5cdd6d91a26903f4cc973266c5fc
avvadukkam <davvadukkam@gmail.com>
2024-12-25 21:40:07 -0500

    12/25/2024

--- 2251-2500/leetcode2255.py
+++ 2251-2500/leetcode2255.py
@@ -7,3 +7,9 @@ class Solution:
             if s[:i+1] in words_dic:
                 count += words_dic[s[:i+1]]
         return count
+    
+
+# Optimized
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        return sum(1 for word in words if s.startswith(word))

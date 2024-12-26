f6df78b86388b20ee0c84cef7dc0e5584585ca07
avvadukkam <davvadukkam@gmail.com>
2024-12-25 22:01:47 -0500

    12/25/2024

--- 2251-2500/leetcode2264.py
+++ 2251-2500/leetcode2264.py
@@ -14,3 +14,9 @@ class Solution:
             if val in num:
                 return val
         return ""
+    
+# One-liner
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        dic = ["999","888", "777", "666","555", "444", "333", "222", "111", "000"]
+        return next((val for val in dic if val in num), "")

8bbafdb588bc6af97c27710ed5ad761e1baf5970
avvadukkam <davvadukkam@gmail.com>
2024-12-25 21:59:31 -0500

    12/25/2024

--- 2251-2500/leetcode2264.py
+++ 2251-2500/leetcode2264.py
@@ -5,3 +5,12 @@ class Solution:
             if num[i] == num[i+1] == num[i+2]:
                 max_int = max(max_int, num[i:i+3])
         return max_int
+    
+# OR
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        dic = ["999","888", "777", "666","555", "444", "333", "222", "111", "000"]
+        for val in dic:
+            if val in num:
+                return val
+        return ""

5ed4a5bf6804d1559ee5e81c7f37b3c7678cf62f
avvadukkam <davvadukkam@gmail.com>
2024-12-28 23:15:30 -0500

    12/28/2024

---
+++ 2251-2500/leetcode2315.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        count = 0
+        in_pair = False
+
+        for char in s:
+            if char == '|':
+                in_pair = not in_pair
+            elif char == '*' and not in_pair:
+                count += 1
+        
+        return count

f0e6384c697d8124152af900106ace46d9f5318c
avvadukkam <davvadukkam@gmail.com>
2024-12-30 22:03:08 -0500

    12/30/2024

--- 2251-2500/leetcode2367.py
+++ 2251-2500/leetcode2367.py
@@ -9,3 +9,13 @@ class Solution:
                         count += 1
         return count
     
+# Optimized
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        count = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            if num + diff in num_set and num + 2 * diff in num_set:
+                count += 1
+        return count

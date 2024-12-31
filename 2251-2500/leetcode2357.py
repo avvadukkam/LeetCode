73053bd331c907160e648b7df3776516b3f979b9
avvadukkam <davvadukkam@gmail.com>
2024-12-30 21:54:10 -0500

    12/30/2024

--- 2251-2500/leetcode2357.py
+++ 2251-2500/leetcode2357.py
@@ -8,3 +8,12 @@ class Solution:
         
         return step
     
+#Optimized
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        s = set(nums)
+
+        if 0 in s:
+            s.remove(0)
+        
+        return len(s)
